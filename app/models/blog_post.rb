eff89d0a04ae466aa5c21ea20071afc45b8f6321
Bhaskar <bhaskar8088@gmail.com>
2013-09-06 03:51:09 +0530

    Added def short_content for model BlogPost

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,8 @@
 class BlogPost < ActiveRecord::Base
     belongs_to :author, class_name: "User", foreign_key: "author_id"
+
+    def short_content
+        ActionView::Base.full_sanitizer.sanitize(self.content).slice(0, 100)
+    end
+
 end
