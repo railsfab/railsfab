61b1acc1ae8cfe378772441285eb7c6b90bff9e3
Bhaskar <bhaskar8088@gmail.com>
2013-09-06 05:41:12 +0530

    Added some validations to blog_post. And showing errors in view

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,5 +1,7 @@
 class BlogPost < ActiveRecord::Base
     belongs_to :author, class_name: "User", foreign_key: "author_id"
+    validates :title, presence: true
+    validates :content, presence: true
 
     def short_content
         ActionView::Base.full_sanitizer.sanitize(self.content).slice(0, 100)
