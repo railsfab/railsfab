491712e61c6008ad649816d1368d0100c9022b79
Bhaskar <bhaskar8088@gmail.com>
2013-09-07 16:31:52 +0530

    Schema after migrating to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20130905201548) do
     t.integer  "author_id"
   end
 
-  add_index "blog_posts", ["author_id"], name: "index_blog_posts_on_author_id"
+  add_index "blog_posts", ["author_id"], name: "index_blog_posts_on_author_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20130905201548) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
