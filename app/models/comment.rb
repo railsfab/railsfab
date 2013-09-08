1337b2da4aab23863ce685881a29624a7db870c5
Bhaskar <bhaskar8088@gmail.com>
2013-09-08 15:46:31 +0530

    Email validation while commenting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
     validates :name, presence: true
     validates :comment, presence: true
     validates :email, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
