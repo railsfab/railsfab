86609f2cb1e420b2eedf16ad09dd0a539e85ea4a
Bhaskar <bhaskar8088@gmail.com>
2013-11-13 03:52:28 +0530

    Added first_name, last_name to User

--- db/migrate/20130905193659_devise_create_users.rb
+++ db/migrate/20130905193659_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
+      ## More columns
+      t.string :first_name
+      t.string :last_name
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
