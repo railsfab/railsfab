f0f6802173d062057adaf3a5d1bc740caafcc221
Bhaskar <bhaskar8088@gmail.com>
2013-11-13 10:27:14 +0530

    Added username to user model, added model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,28 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, class_name: BlogPost, foreign_key: :author_id
+
+  before_save :populate_username
+
+  def populate_username
+      username = self.email.split("@")[0]
+      objects = User.where(username: username)
+      if objects.count > 0
+          username = "#{username}#{rand(111111..999999)}"
+      end
+      self.username = username
+  end
+
+  def available_name
+      full_name = "#{self.first_name} #{self.last_name}"
+      if full_name.strip.length > 0
+          full_name
+      else
+          self.email.split("@")[0]
+      end
+  end
+
+
 end
--- db/migrate/20130905193659_devise_create_users.rb
+++ db/migrate/20130905193659_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, :null => false, :default => ""
 
       ## More columns
+      t.string :username
       t.string :first_name
       t.string :last_name
 
