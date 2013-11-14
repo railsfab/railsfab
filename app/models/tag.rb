979aac9cb1609c4a02115e1e07337d3d5bbaaeef
Bhaskar <bhaskar8088@gmail.com>
2013-11-14 10:19:05 +0530

    Category, tag pages

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
+    has_many :post_tag_relations
+    has_many :posts, class_name: BlogPost, through: :post_tag_relations, uniq: true
+
     validate :create_and_validate_slug
     validates :slug, :uniqueness => { 
         message: ": Category with this name already exists"}
