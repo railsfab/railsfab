856b27653eb1075e74abdaae0c9f89a8fe052fb2
Bhaskar <bhaskar8088@gmail.com>
2013-11-14 08:45:48 +0530

    Tag crud

---
+++ app/controllers/tag_controller.rb
@@ -0,0 +1,30 @@
+class TagController < ApplicationController
+   def new
+        @tag = Tag.new
+    end
+
+    def create
+        @tag = Tag.new new_tag_params
+        if @tag.valid?
+            @tag.save
+            redirect_to tag_show_path(slug: @tag.slug)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @categories = Tag.all
+    end
+
+    def show
+        @tag = Tag.find_by_slug(params[:slug])
+    end
+
+    private
+
+    def new_tag_params
+        params.require(:tag).permit(:name)
+    end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ActiveRecord::Base
+    validate :create_and_validate_slug
+    validates :slug, :uniqueness => { 
+        message: ": Category with this name already exists"}
+
+    paginates_per 10
+
+    def create_and_validate_slug
+        self.slug = self.name.parameterize
+    end
+
 end
