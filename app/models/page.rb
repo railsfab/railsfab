6cf6000fdc18e803536ebe8d0f04713fbe0d3617
Bhaskar <bhaskar8088@gmail.com>
2013-11-30 02:07:43 +0530

    Added model validations, code to generate and save slug

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,10 @@
 class Page < ActiveRecord::Base
+    validates :name, presence: true
+    validate :create_and_validate_slug
+    validates :slug, :uniqueness => { 
+        message: ": Page with this name already exists. Please choose another. " }
+
+    def create_and_validate_slug
+        self.slug = self.name.parameterize
+    end
 end
